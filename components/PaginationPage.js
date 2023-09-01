421a8c3de90fb1d4a0fe67d4cc1d3d55a34cdc93
marces <goat@marces.dev>
2023-09-01 07:55:30 -0300

    responsive style

--- components/PaginationPage.js
+++ components/PaginationPage.js
@@ -25,7 +25,7 @@ const PaginationPage = ({ currentPage, totalItems, perPage, items }) => {
         itemsPerPage={perPage}
         renderPageLink={(page) => `/${base}/${page}`}
       />
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {sortedItems.map((item) => (
           <Card key={item.name} {...item} />
         ))}
